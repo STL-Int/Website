795b6cfc8ea3dba26eb07ce09bfaf36865975cf8
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2021-07-28 16:47:33 +0100

    Added title trimming to news post card

--- src/components/newsPostCard/newsPostCard.js
+++ src/components/newsPostCard/newsPostCard.js
@@ -6,6 +6,10 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import "./newsPostCardStyling.scss"
 
 const NewsPostCard = ({ imgSrc, imgAlt, slug, title, date, excerpt }) => {
+  const max_title_chars = 36
+  if (title.length > max_title_chars){
+    title = title.substring(0, max_title_chars) + " ..."
+  }
   return (
     <div className="post-wrapper">
       <Link to={`/blog/${slug}/`} className="post">
