67f7aa44378cdb88506efb2d04481cff018748a1
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2021-07-30 10:04:24 +0100

    resolve helmet path

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
+var { Helmet } = require.resolve("react-helmet")
 import { useStaticQuery, graphql } from "gatsby"
 
 function SEO({ description, lang, meta, title }) {
