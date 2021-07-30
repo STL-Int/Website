50470e14448abd1d7a6199862469c52ed099785b
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2021-07-30 12:57:02 +0100

    installed core-js

--- src/components/NavBar/Logo.js
+++ src/components/NavBar/Logo.js
@@ -3,6 +3,7 @@ import React from "react"
 import { GatsbyImage } from "gatsby-plugin-image";
 import { Link, useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
+import 'core-js'; 
 
 const LogoWrapper = styled.View`
   width: 80px;
