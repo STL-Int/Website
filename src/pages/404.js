b28cc16f46529758eb667f320a27b9f40a458deb
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2021-09-02 08:27:46 +0100

    Buttons

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "./pageStyles/404.scss"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <div className="error-page-wrapper">
-      <div className="error-title-wrapper">
-        <h1 className="error-title">404</h1>
+function NotFoundPage () {
+  return (
+    <Layout> 
+      <SEO title="404: Not found" />
+      <div className="error-page-wrapper">
+        <div className="error-title-wrapper">
+          <h1 className="error-title">404</h1>
+        </div>
+        <div className="error-message-wrapper">
+          <h2 className="error-message">
+            The Page You Requested Could Not Be Found
+          </h2>
+        </div>
+      
+        <div className="error-button-wrapper">
+          <button 
+            className="btn-white error-button" 
+            onClick={() => navigate(-1)}
+          >
+            <p>Go Back</p>
+          </button>
+          <button 
+            className="btn-white error-button" 
+            onClick={() => navigate("/")}
+          >
+            <p>Go To Homepage</p>
+          </button>
+        </div>
       </div>
-      <div className="error-message-wrapper">
-        <h2 className="error-message">
-          The Page You Requested Could Not Be Found
-        </h2>
-      </div>
-      <div className="error-button-wrapper">
-        <Link id="error-button" type="button" className="button" to="/">
-          Back To Homepage
-        </Link>
-      </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default NotFoundPage
