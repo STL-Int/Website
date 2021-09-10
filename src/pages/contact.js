97ff451dc6d806a032fa3092ab1a652a96e462d2
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2021-09-10 11:30:39 +0100

    make formspree form with react

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,65 +1,87 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useForm, ValidationError } from '@formspree/react';
 
 import "./pageStyles/contact.scss"
 
-export default function Contact() {
-  return (
-    <Layout>
-      <SEO title="Contact Us" />
-      <div className="contact-page-content">
-        <h1 className="heading-contact-page">How can we help?</h1>
-
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mgerrqlz");
+  if (state.succeeded) {
+      return (
         <form
-          className="contact-form"
-          method="post"
-          action="https://formspree.io/f/mgerrqlz "
+            id="contact"
+            className="contact-form form-success"
+            method="post"
+            onSubmit={handleSubmit}
         >
-          <div className="name-wrapper">
-            <label htmlFor="name">Full name</label>
-            <input
-              id="name"
-              type="text"
-              name="fullname"
-              required
-              className="input"
-            />
-            <p className="sub-text">e.g. John Smith</p>
-          </div>
-
-          <div className="email-wrapper">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              required
-              className="input"
-            />
-            <p className="sub-text">e.g. you@example.com</p>
-          </div>
-
-          <div className="message-wrapper">
-            <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              name="message"
-              required
-              className="input"
-            ></textarea>
-          </div>
-
-          <button className="submit-button" type="submit">
-            <h3>Send</h3>
-            <span className="arrow">↦</span>
-          </button>
-
-          <input type="hidden" name="_recaptcha" id="_recaptcha"/>
-          <script src="https://www.google.com/recaptcha/api.js?render=6Lf7UsoUAAAAACT2Z6gLyh7RTDfyYGxfZ-M4D0ph"></script>
-          <script src="https://www.flexyform.com/js/recaptcha.js"></script>
+            <h2>Thanks for getting in contact, we will be in touch as soon as possible!</h2>
         </form>
-      </div>
-    </Layout>
-  )
+      );
+  }
+  return (
+    <form
+        id="contact"
+        className="contact-form"
+        method="post"
+        onSubmit={handleSubmit}
+    >
+        <div className="name-wrapper">
+        <label htmlFor="name">Full name</label>
+        <input
+            id="name"
+            type="text"
+            name="fullname"
+            required
+            className="input"
+        />
+        <p className="sub-text">e.g. John Smith</p>
+        </div>
+
+        <div className="email-wrapper">
+        <label htmlFor="email">Email</label>
+        <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            className="input"
+        />
+        <p className="sub-text">e.g. you@example.com</p>
+        <ValidationError field="email" prefix="Email" errors={state.errors} />
+        </div>
+
+        <div className="message-wrapper">
+        <label htmlFor="message">Message</label>
+        <textarea
+            id="message"
+            name="message"
+            required
+            className="input"
+        />
+        <ValidationError 
+            prefix="Message" 
+            field="message"
+            errors={state.errors}
+        />
+        </div>
+
+        <button className="submit-button" type="submit" disabled={state.submitting}>
+        <h3>Send</h3>
+        <span className="arrow">↦</span>
+        </button>
+    </form>
+  );
 }
+function Contact() {
+    return (
+        <Layout>
+            <SEO title="Contact Us" />
+            <div className="contact-page-content">
+                <h1 className="heading-contact-page">How can we help?</h1>
+                <ContactForm />
+            </div>
+        </Layout>
+    );
+}
+export default Contact;
