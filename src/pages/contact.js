00b31d0757690c01480f46d206ca6a6a4d136fbb
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2021-09-13 22:19:03 +0100

    formatting and removing thank you text

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,89 +1,63 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { useForm, ValidationError } from '@formspree/react';
 
 import "./pageStyles/contact.scss"
 
 function ContactForm() {
-  const [state, handleSubmit] = useForm("mgerrqlz");
-  if (state.succeeded) {
-      return (
-        <form
-            id="contact"
-            className="contact-form form-success"
-            method="post"
-            onSubmit={handleSubmit}
-        >
-            <h2>Thanks for getting in contact, we will be in touch as soon as possible!</h2>
-        </form>
-      );
-  }
-  
   return (
     // TODO: Add subject field
     <form
-        id="contact"
-        className="contact-form"
-        method="post"
-        action="https://formsubmit.co/ruben.heyse@icloud.com"
+      id="contact"
+      className="contact-form"
+      method="post"
+      action="https://formsubmit.co/ruben.heyse@icloud.com"
     >
-        <div className="name-wrapper">
+      <div className="name-wrapper">
         <label htmlFor="name">Full Name</label>
-        <input
-            id="name"
-            type="text"
-            name="name"
-            required
-            className="input"
-        />
+        <input id="name" type="text" name="name" required className="input" />
         <p className="sub-text">e.g. John Smith</p>
-        </div>
+      </div>
 
-        <div className="email-wrapper">
+      <div className="email-wrapper">
         <label htmlFor="email">Email</label>
         <input
-            id="email"
-            type="email"
-            name="email"
-            required
-            className="input"
+          id="email"
+          type="email"
+          name="email"
+          required
+          className="input"
         />
         <p className="sub-text">e.g. you@example.com</p>
-        <ValidationError field="email" prefix="Email" errors={state.errors} />
-        </div>
+      </div>
 
-        <div className="message-wrapper">
+      <div className="message-wrapper">
         <label htmlFor="message">Message</label>
-        <textarea
-            id="message"
-            name="message"
-            required
-            className="input"
-        />
-        <ValidationError 
-            prefix="Message" 
-            field="message"
-            errors={state.errors}
-        />
-        </div>
+        <textarea id="message" name="message" required className="input" />
+      </div>
 
-        <button className="submit-button" type="submit" disabled={state.submitting}>
+      <button className="submit-button" type="submit">
         <h3>Send</h3>
         <span className="arrow">↦</span>
-        </button>
+      </button>
+
+      <input
+        type="hidden"
+        name="_next"
+        value="https://www.stl-int.uk/formsuccess/"
+      ></input>
     </form>
-  );
+  )
 }
 function Contact() {
-    return (
-        <Layout>
-            <SEO title="Contact Us" />
-            <div className="contact-page-content">
-                <h1 className="heading-contact-page">How can we help?</h1>
-                <ContactForm />
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <SEO title="Contact Us" />
+      <div className="contact-page-content">
+        <h1 className="heading-contact-page">How can we help?</h1>
+        <ContactForm />
+      </div>
+    </Layout>
+  )
 }
-export default Contact;
+export default Contact
